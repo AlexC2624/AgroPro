59cc3152e88c24bd69e8dc820893648d8b5f56b4
alex2624 <alex.cargnin.2006@gmail.com>
2025-03-04 23:18:05 -0300

    4 de março - final

--- back.py
+++ back.py
@@ -2,40 +2,66 @@
 class Gerenciador:
     def __init__(self, nome_do_banco):
         """
-
         Args:
             nome_do_banco (string): O nome do banco de dados a ser manipulado.
         """
-        pass
-    def salvar_registro(self, valores):
-        """
-        Ajusta os dados para serem salvos
+        self.cBanco = Banco(nome_do_banco)
 
-        Args:
-            volores (list[list]): Cada lista dentro da lista representa uma linha.
+    def verif_duplicidade(self, coluna, valor):
         """
-        sucesso = False
+        Args:
+            coluna (int): Id da coluna a procurar no arquivo.
+            valor (strg/int): Pode ser uma string ou um integer que será para comparar.
+        
+        Returns:
+            list: Lista com as linhas em que foi encontrado.
+        """
         try:
-            print(valores)
-            banco = Banco('data.csv')
-            print(banco.ler())
-            input()
-            sucesso = True
-        except:
-            sucesso = []
-        return sucesso
+            dados_salvos = self.cBanco.ler()
+            cont_linha = 0  # Indica a linha atual dentro do for
+            ocorencias = [] # Armazena as ocoerencias
+            for linha in dados_salvos:
+                if linha[coluna] == valor:
+                    ocorencias.append(cont_linha)
+                cont_linha += 1
+            return ocorencias
+        except FileNotFoundError:
+            return []
+        
+    def salvar_registro(self, valores, verificar_duplicidade=[None]):
+        """
+        Args:
+            valores (list): Lista com os dados a serem salvos, a lista representa a linha.
+            verificar_duplicidade (list, optional): Índices das colunas para verificar duplicatas. Defaults to None.
+        """
+        indice = 0
+        if verificar_duplicidade[0] != None:
+            for idx in verificar_duplicidade:
+                verificacao = self.verif_duplicidade(idx, valores[idx])
+                if len(verificacao) != 0:
+                    return False, idx
 
 class Banco:
     def __init__(self, nome_banco_data):
         self.banco_nome = nome_banco_data
     def ler(self):
-        try:
-            with open(self.banco_nome, 'r') as arquivo_r:
-                return arquivo_r.read()
-        except FileNotFoundError:
-            with open(self.banco_nome, 'w') as arquivo_w:
-                arquivo_w.write('1')
-            return 'escrita'
+        """Lê o arquivo csv
+
+        Returns:
+            list[list]: Cada linha estará dentro da lista que, por sua vez estará dentro da da lista principal.
+
+        Raises:
+            FileNotFoundError: Pode retornar se o arquivo não existir.
+        """
+        with open(self.banco_nome, 'r') as arquivo_r:
+            return arquivo_r.read()
                 
-    def escrever(self):
-        pass
+    def adicionar(self, dados):
+        """
+        Args:
+            dados (list[list]): Dados para serem escritos no arquivo
+        """
+        with open(self.banco_nome, 'a') as arquivo_w:
+            for linha in dados:
+                arquivo_w.write(linha + ', ')
+            arquivo_w.write('\n')
